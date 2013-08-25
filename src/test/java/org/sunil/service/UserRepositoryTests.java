d60e199a895ec76fed32eba01bbe93fa9b67563f
sunil kalyanpur <kalyanpursunil85@gmail.com>
2013-08-24 19:42:08 -0500

    updated

--- src/test/java/org/sunil/service/UserRepositoryTests.java
+++ src/test/java/org/sunil/service/UserRepositoryTests.java
@@ -9,17 +9,21 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 import org.sunil.dao.UserDao;
 import org.sunil.model.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations="classpath:applicationContextTest.xml")
+@ContextConfiguration(locations={"classpath:applicationContextTest.xml","classpath:SpringWithMockito-ServletTest.xml"})
 @Transactional
 public class UserRepositoryTests {
 	
 	@Autowired
 	protected UserDao userDao;
+	
+	@Autowired
+	private WebApplicationContext wac;
 	
 	@Test
 	public void validateUser(){
